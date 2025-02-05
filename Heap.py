e0831c515f788ab9f836271d0c2cdd7dab1ce303
Sebastián Martínez <sebmartinezse@unal.edu.co>
2025-02-04 22:10:22 -0500

    No he podido

--- Heap.py
+++ Heap.py
@@ -18,13 +18,13 @@ class Heap():
         self.__arreglo = arreglo
     
     def parent(self, i):
-        return round((i/2) - 1)
+        return round((float(self.__arreglo(i/2))) - 1)
 
 h1 = Heap(0, None)
 arreglo1 = [12, 31, 24, 9, 11]
 h1.setArreglo(arreglo1)
-#print(h1.parent(6))
-print(round(h1.getArreglo()[]))
+print(h1.parent(1))
+#print(h1.self.__arreglo)
 
     
     
